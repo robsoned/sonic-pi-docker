8a1ba0d96de1144d825d998bc746bbd6a8f1b200
robsoned <robsoneduardosantos@gmail.com>
2024-07-13 14:57:01 -0300

    Update build script to clean up after dependencies installation

--- build-scripts/install-apt-dependencies.sh
+++ build-scripts/install-apt-dependencies.sh
@@ -3,4 +3,6 @@
 set -eou pipefail
 
 apt-get update \
-    && apt-get install -y ${APT_DEPENDENCIES}
+    && apt-get install -y ${APT_DEPENDENCIES} \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
